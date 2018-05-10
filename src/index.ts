0d8f9ef67936b0a7227e8db3df9c29b5fc46d9e8
ShiranuiNui <shiranuisayaka@gmail.com>
2018-05-11 00:36:51 +0900

    [Add] Import TCP Socket Library

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("HELLO WORLD");
+import * as net from "net";
+
+const server = net.createServer((socket) => {
+    socket.write("HELLO TCP WORLD");
+    socket.end();
+});
+server.listen(50017, () => {
+    console.log("SERVER ONLINE");
+});
